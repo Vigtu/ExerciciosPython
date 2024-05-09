086156b55201b2c9b92209ea8902135b4c442492
VICTOR CORREA GOMES <victor.cgomes@uvvnet.com.br>
2024-05-08 21:09:42 -0300

    add

---
+++ 011.py
@@ -0,0 +1,13 @@
+class Polygon:
+    def __init__(self, sides, name):
+        self.sides = sides
+        self.name = name
+
+square = Polygon(4, "Square")
+pentagon = Polygon(5, "Pentagon")
+
+print(square.sides)
+print(square.name)
+
+print(pentagon.sides)
+print(pentagon.name)
